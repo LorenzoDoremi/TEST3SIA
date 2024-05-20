7d4b894ca59a2ca22fa222ae4e1f4cb23d08fe83
Lorenzo Doremi <73567993+LorenzoDoremi@users.noreply.github.com>
2024-05-20 13:22:56 +0200

    carico i primi file del sito web

---
+++ script.js
@@ -0,0 +1,18 @@
+let b = 12;
+let h = 8;
+let area = b*h/2;
+/* aumento di 10 l'area */
+area -= 10;
+area++; // aggiungi 1   += 1
+area--; // sottrai 1    -= 1
+document.getElementById("x").innerText = area;
+// dichiara 2 variabili e stampa il quadrato della somma. 
+let a = 10;
+let b = 7;
+let risultato = (a+b)*(a+b);
+document.getElementById("x").innerText = risultato;
+// media 
+let media = (a+b+risultato)/3;
+document.getElementById("x").innerText = media;
+
+
